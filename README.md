25e91d4e17d54eca2c2c6e759f3752b5256ae2a5
Kevin Mulligan <kevin.john.mulligan@gmail.com>
2020-10-10 20:44:25 +0800

    First commit

--- README.md
+++ README.md
@@ -1,68 +1,36 @@
-This project was bootstrapped with [Create NW.js React App](https://github.com/naviapps/create-nw-react-app).
+# NUS4all
 
-## Available Scripts
+*NUS4all* is a BLE client that is a platform agnostic terminal for NUS - Nordicsemiconductor Uart Services (BLE UART) which connects to any device that supports NUS rather than specifically servicing particular hardware. Other solutions tend to serve specific hardware and require a specific setup. *NUS4all* aims to be as generic as possible, providing a pure terminal experience over BLE NUS.
 
-In the project directory, you can run:
+### Terminology:
+**NUS:** Nordicsemiconductor Uart Services
+**BLE:** Bluetooth Low Energy, Bluetooth 4.0, Bluetooth 5.0
+**BLE client/central:** A PC/mobile device that connects to BLE peripherals to collect data and send commands. The central device connects as a client to a BLE peripheral.
+**BLE server/peripheral:** The small device that performs actions in the world. The peripheral device runs the BLE server.
+**UART:** Universal Asynchronous Receiver-Transmitter
+**Platform Agnostic:** Runs on Windows/Linux/macOS
 
-### `npm start`
+### Technology:
+This project is based on [web-device-cli](https://github.com/makerdiary/web-device-cli) which makes use of the Web Bluetooth protocol. Additions were made using [React](https://reactjs.org/) and [NWjs](https://nwjs.io/) to convert web-device-cli into an agnostic desktop application.
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+##### To be implemented:
+TCP backend:
+- *NUS4all* will have a TCP server that allows any **local** connection to the NUS on the BLE peripheral
+- *NUS4all* will have a TCP server that allows any **remote** connection to the NUS on the BLE peripheral
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+#### Etymology:
 
-### `npm run build`
+*What's in a name? **(too much)***
+I originally thought of using **NUS4all** but found that this was used as a political hashtag so I dropped it.
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Next I went for ***agnostic NUS***  written as ***ag-NUS***
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+- agnus means lamb in latin
+- ag means silver on the period table of elements
+- therefore I could have used a silver lamb to represent the project, but this seemed too religious given the project is meant to be agnostic after all :stuck_out_tongue_winking_eye:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+After that ***JustNUS Terminal*** - Just this. Seemed a bit bland.
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Finally I settled on ***NeutralNUS Terminal*** - pronounced neutralness  (ˈnjuːtrəlnəs)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,391 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from "react"
+import './App.sass'
+import './styles.scss'
+
+const bleNusServiceUUID  = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+const bleNusCharRXUUID   = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+const bleNusCharTXUUID   = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+const MTU = 20;
+
+var bleDevice;
+var nusService;
+var rxCharacteristic;
+var txCharacteristic;
+
+var connected = false;
+
+const net = require('net');
+const readline = require('readline');
+const fs = require('fs')
+//define the server port and host
+const port = 8080;
+const host = '127.0.0.1';
+// let sock = undefined;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [consoleText, setConsoleText] = useState("")
+    const [inputText, setInputText] = useState("")
+    const [suggestionChecked, setSuggestionChecked] = useState(true)
+    const textBoxRef = useRef(null)
+    const sock = useRef(null)
+
+    function createSocket(socket){
+        sock.current = socket
+    } 
+
+    function setupTcpServer(){
+        //Create an instance of the server
+        const server = net.createServer((s)=>{
+            createSocket(s)
+            onClientConnection(s)
+        });
+        //Start listening with the server on given port and host.
+        server.listen(port,host,function(){
+            console.log(`Server started on port ${port} at ${host}`); 
+        });
+        //Declare connection listener function
+        function onClientConnection(socket){
+            //Log when a client connnects.
+            console.log(`${socket.remoteAddress}:${socket.remotePort} Connected`);
+            //Listen for data from the connected client.
+            socket.on('data',function(data){
+                //Log data from the client
+                console.log(`${socket.remoteAddress}:${socket.remotePort} Says : ${data} `);
+                addConsoleText(`${socket.remoteAddress}:${socket.remotePort} Says : ${data} `);
+                
+                //Send back the data to the client.
+                socket.write(`You fdsfs  ${data}`);
+            });
+            
+            const fileStream = fs.createReadStream('C:/NUSinput.txt');
+
+            const rl = readline.createInterface({
+              input: fileStream,
+              crlfDelay: Infinity
+            });
+            rl.on('line', (line) => {
+                console.log(`readfromfile: ${line}\n`);
+                socket.write(`readfromfile: ${line}\n`);
+            });
+            rl.on('close', () => {
+                socket.end();
+            });
+            
+            // if newdata (data)=>
+            //     socket.write(data)
+            
+            //Handle client connection termination.
+            socket.on('close',function(){
+                console.log(`${socket.remoteAddress}:${socket.remotePort} Terminated the connection`);
+            });
+            //Handle Client connection error.
+            socket.on('error',function(error){
+                console.error(`${socket.remoteAddress}:${socket.remotePort} Connection Error ${error}`);
+            });
+
+        };
+    }
+    
+    function tcpSendString(data){
+        fs.appendFile('C:/NUSinput.txt',`tcpSendString  ${data}`, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+        
+    }
+
+    useEffect(()=>{
+        console.log("setup server")
+        setupTcpServer()
+    }, [])
+
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+    function connectionToggle() {
+        if (connected) {
+            disconnect();
+        } else {
+            connect();
+        }
+    }
+
+    function clearConsole(){
+        setConsoleText("")
+    }
+
+    function connect() {
+        if (!navigator.bluetooth) {
+            console.log('WebBluetooth API is not available.\r\n' +
+                        'Please make sure the Web Bluetooth flag is enabled.');
+            addConsoleText('WebBluetooth API is not available on your browser.\r\n' +
+                        'Please make sure the Web Bluetooth flag is enabled.');
+            return;
+        }
+        console.log('Requesting Bluetooth Device...');
+        navigator.bluetooth.requestDevice({
+            // Search for all devices that support the NUS service and my watch as a functionality test
+            filters: [{services: [bleNusServiceUUID]}, {name: "Amazfit Bip Watch"}]
+            // optionalServices: [bleNusServiceUUID],
+            // acceptAllDevices: true
+        })
+        .then(device => {
+            bleDevice = device; 
+            console.log('Found ' + device.name);
+            console.log('Connecting to GATT Server...');
+            bleDevice.addEventListener('gattserverdisconnected', onDisconnected);
+            return device.gatt.connect();
+        })
+        .then(server => {
+            console.log('Locate NUS service');
+            return server.getPrimaryService(bleNusServiceUUID);
+        })
+        .then(service => {
+            nusService = service;
+            console.log('Found NUS service: ' + service.uuid);
+        })
+        .then(() => {
+            console.log('Locate RX characteristic');
+            return nusService.getCharacteristic(bleNusCharRXUUID);
+        })
+        .then(characteristic => {
+            rxCharacteristic = characteristic;
+            console.log('Found RX characteristic');
+        })
+        .then(() => {
+            console.log('Locate TX characteristic');
+            return nusService.getCharacteristic(bleNusCharTXUUID);
+        })
+        .then(characteristic => {
+            txCharacteristic = characteristic;
+            console.log('Found TX characteristic');
+        })
+        .then(() => {
+            console.log('Enable notifications');
+            return txCharacteristic.startNotifications();
+        })
+        .then(() => {
+            console.log('Notifications started');
+            txCharacteristic.addEventListener('characteristicvaluechanged',
+                                            handleNotifications);
+            connected = true;
+        addConsoleText('\r\n' + bleDevice.name + ' Connected.\r\n');
+            nusSendString('\r');
+        })
+        .catch(error => {
+            console.log('' + error);
+        addConsoleText('' + error);
+            if(bleDevice && bleDevice.gatt.connected)
+            {
+                bleDevice.gatt.disconnect();
+            }
+        });
+    }
+
+    function disconnect() {
+        if (!bleDevice) {
+            console.log('No Bluetooth Device connected...');
+            return;
+        }
+        console.log('Disconnecting from Bluetooth Device...');
+        if (bleDevice.gatt.connected) {
+            bleDevice.gatt.disconnect();
+            connected = false;
+            console.log('Bluetooth Device connected: ' + bleDevice.gatt.connected);
+        } else {
+            console.log('> Bluetooth Device is already disconnected');
+        }
+    }
+
+
+    function onDisconnected() {
+        connected = false;
+    addConsoleText('\r\n' + bleDevice.name + ' Disconnected.\r\n');
+    }
+
+    function handleNotifications(event) {
+        console.log('notification');
+        let value = event.target.value;
+        // Convert raw data bytes to character values and use these to 
+        // construct a string.
+        let str = "";
+        for (let i = 0; i < value.byteLength; i++) {
+            str += String.fromCharCode(value.getUint8(i));
+        }
+        
+        addConsoleText(`<RX: ${str}`); 
+    }
+
+    function nusSendString(s) {
+        if(bleDevice && bleDevice.gatt.connected) {
+            console.log("send: " + s);
+            let val_arr = new Uint8Array(s.length)
+            for (let i = 0; i < s.length; i++) {
+                let val = s[i].charCodeAt(0);
+                val_arr[i] = val;
+            }
+            sendNextChunk(val_arr);
+        } else {
+            console.log('Not connected to a device yet.');
+        }
+    }
+
+    function sendNextChunk(a) {
+        let chunk = a.slice(0, MTU);
+        rxCharacteristic.writeValue(chunk)
+        .then(function() {
+            if (a.length > MTU) {
+                sendNextChunk(a.slice(MTU));
+            }
+        });
+    }
+
+
+
+    function initContent() {
+    addConsoleText("Welcome to Web Device CLI V0.1.0 (03/19/2019)\r\nCopyright (C) 2019  makerdiary.\r\n* Source: https://github.com/makerdiary/web-device-cli\r\n\
+    \r\nThis is a React Electron App based on a Web Command Line Interface via NUS (Nordic UART Service) using Web Bluetooth.\r\n\
+    \r\n React Electron adaptation by Kevin John Mulligan.\r\n - - - - - - - - - - - - - - - - - - - - - - - - -");
+    }
+
+    function setupNUS() {
+        initContent()
+    }
+
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+    function addConsoleText(t){
+        textBoxRef.current.scrollTop = textBoxRef.current.scrollHeight
+        // textarea.scrollTop = textarea.scrollHeight;
+        setConsoleText((prevText) => prevText + t + '\n')
+    }
+
+    
+    function handleChange(e) {
+        // handleChange is currently used but the es6 linter wants it
+        e.preventDefault() 
+        const {name,value} = e.target
+        if(name === 'inputText'){   //send when selected from a list
+            console.log(`handleChange: ${name}  ${value}`);
+            send(value)
+        } else if (name === 'sendButton'){
+            send(inputText)
+            console.log(`handleChange: ${name}  ${inputText}`);
+        } else {
+            console.log(`handleChange: ${name}  ${value}`);
+        }
+
+    }
+
+    function handleCheck(){
+        setSuggestionChecked(!suggestionChecked) 
+    }
+
+    function send(data){
+        addConsoleText(`>TX: ${data}`)  //add to local console
+        nusSendString(data)             //send over BLE
+        tcpSendString(data)             //send over TCP
+        setInputText("")                //clear input box
+    }
+
+    function keyPressed(e) {
+        e.preventDefault() 
+        const {key} = e
+        const {value} = e.target;
+
+        setInputText((prevText) => prevText + key)
+        if (key === "Enter") {
+            send(value)
+        }
+      }
+        
+    function handleSubmit(e) {
+        e.preventDefault() 
+    }
+    useEffect(() => {
+        setupNUS() 
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
+    useEffect(() => {
+        console.log(inputText) 
+        nusSendString(inputText);      
+    }, [setInputText])
+
+
+    return (
+        <div className='box'>
+            <div className='heading-box'>
+                <button 
+                className="button is-primary"
+                    onClick={connectionToggle}
+                >
+                    {connected? "Disconnect": "Connect"}
+                </button>
+                
+               <h1 className="title">BLE UART</h1>
+               <h1 className="title" style={{color: "#000"}}>CLI</h1>
+           </div>
+
+            <div className="content-box">
+                <textarea
+                    ref={textBoxRef}
+                    readOnly = {true} 
+                    value={consoleText}
+                />
+            </div>
+
+            <div className="input-box">
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        value={inputText} 
+                        name="inputText" 
+                        className="input is-primary"
+                        autoComplete={suggestionChecked? "on": "off"}
+                        placeholder=">" 
+                        onChange={handleChange} 
+                        onKeyPress={keyPressed}
+                    />
+                </form>
+    
+                <button 
+                        className="button is-info"
+                        name="sendButton" 
+                        onClick={handleChange}
+                    >
+                    Send
+                </button>
+                <button 
+                        className="button is-info"
+                        onClick={clearConsole}
+                    >
+                    Clear
+                </button>
+                <button 
+                className="button is-info"
+                    onClick={handleCheck}
+                >
+                    {suggestionChecked? "Disable Suggestions": "Enable Suggestions"}
+                </button>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
+
+
