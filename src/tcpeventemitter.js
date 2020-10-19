dd9f373aa09433aea9d7c28468f5574a687aae3f
Kevin Mulligan <kevin.john.mulligan@gmail.com>
2020-10-19 20:48:11 +0800

    Add TCP Event Emitter

---
+++ src/tcpeventemitter.js
@@ -0,0 +1,17 @@
+const EventEmitter = require('events')
+
+class TcpEventEmitter extends EventEmitter {
+    send(message) {
+        // Raise an event to indicate that a TCP message must be sent out
+        // Pass the message to be sent as an argument
+        this.emit('sendTcp', message)
+    }
+
+    received(message) {
+         // Raise an event to indicate that a TCP message has been received
+         // Pass the received message contents through as an argument
+        this.emit('receiveTcp', message)
+    }
+}
+
+module.exports = TcpEventEmitter
