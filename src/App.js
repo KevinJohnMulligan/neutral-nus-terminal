b8dafc4fdb598fdd19c80816fdc99bb444e4fa96
Kevin Mulligan <kevin.john.mulligan@gmail.com>
2020-10-22 18:08:58 +0800

    Refactor: Remove BLE functionality from App.js
    - Move all BLE related functionality to App.js to comply with the Single
    Responsiblity Principle of SOLID
    - The BLE module now makes use of events (EventEmitter) which makes it
    more useful to other projects as it can communicate with other modules
    (this is how it interacts with App.js)
    - App.js currently has TCP events and BLE events, it also controls the
    GUI

--- src/App.js
+++ src/App.js
@@ -2,18 +2,8 @@ import React, {useState, useEffect, useRef} from "react"
 import './App.sass'
 import './styles.scss'
 import { utf8ByteArrayToString } from 'utf8-string-bytes'; // this module also has 'stringToUtf8ByteArray'
+import  BleModule from './BleModule.js'
 
-const bleNusServiceUUID  = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
-const bleNusCharRXUUID   = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
-const bleNusCharTXUUID   = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
-const MTU = 20;
-
-var bleDevice;
-var nusService;
-var rxCharacteristic;
-var txCharacteristic;
-
-var connected = false;
 
 const net = require('net');
 //define the server port and host
@@ -28,6 +18,8 @@ var win = nw.Window.get();
 // Open the Dev Tools every time (used for debugging only), it defaults to the console which is what we need
 win.showDevTools()
 
+const bleMod =new BleModule()
+
 function App() {
     const [consoleText, setConsoleText] = useState("")
     const [inputText, setInputText] = useState("")
@@ -36,6 +28,25 @@ function App() {
     const sock = useRef(null)
 
     useEffect(()=>{
+        // Create a BLE instance
+        console.log("setup setConnected listener")
+        bleMod.on('setConnected', (data) => {
+            console.log('setConnected: ', data)
+            console.log(bleMod.connected)
+        })
+
+        console.log("setup BLE send listener")
+        bleMod.on('sendBle', (data) => {
+            console.log('------- BLE send event: ', data)
+            send(data)
+        })
+
+        console.log("setup BLE addConsoleText listener")
+        bleMod.on('addConsoleText', (data) => {
+            console.log('------- BLE addConsoleText event: ', data)
+            addConsoleText(data)
+        })
+
         console.log("setup TCP receive listener")
         // Register a listener for messages received via TCP
         tcpevents.on('receivedTcp', (data) => {
@@ -117,144 +128,9 @@ function App() {
 // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
-    function connectionToggle() {
-        if (connected) {
-            disconnect();
-        } else {
-            connect();
-        }
-    }
-
     function clearConsole(){
         setConsoleText("")
     }
-
-    function connect() {
-        if (!navigator.bluetooth) {
-            console.log('WebBluetooth API is not available.\r\n' +
-                        'Please make sure the Web Bluetooth flag is enabled.');
-            addConsoleText('WebBluetooth API is not available on your browser.\r\n' +
-                        'Please make sure the Web Bluetooth flag is enabled.');
-            return;
-        }
-        console.log('Requesting Bluetooth Device...');
-        navigator.bluetooth.requestDevice({
-            // Search for all devices that support the NUS service and my watch as a functionality test
-            filters: [{services: [bleNusServiceUUID]}, {name: "Amazfit Bip Watch"}]
-            // optionalServices: [bleNusServiceUUID],
-            // acceptAllDevices: true
-        })
-        .then(device => {
-            bleDevice = device; 
-            console.log('Found ' + device.name);
-            console.log('Connecting to GATT Server...');
-            bleDevice.addEventListener('gattserverdisconnected', onDisconnected);
-            return device.gatt.connect();
-        })
-        .then(server => {
-            console.log('Locate NUS service');
-            return server.getPrimaryService(bleNusServiceUUID);
-        })
-        .then(service => {
-            nusService = service;
-            console.log('Found NUS service: ' + service.uuid);
-        })
-        .then(() => {
-            console.log('Locate RX characteristic');
-            return nusService.getCharacteristic(bleNusCharRXUUID);
-        })
-        .then(characteristic => {
-            rxCharacteristic = characteristic;
-            console.log('Found RX characteristic');
-        })
-        .then(() => {
-            console.log('Locate TX characteristic');
-            return nusService.getCharacteristic(bleNusCharTXUUID);
-        })
-        .then(characteristic => {
-            txCharacteristic = characteristic;
-            console.log('Found TX characteristic');
-        })
-        .then(() => {
-            console.log('Enable notifications');
-            return txCharacteristic.startNotifications();
-        })
-        .then(() => {
-            console.log('Notifications started');
-            txCharacteristic.addEventListener('characteristicvaluechanged',
-                                            handleNotifications);
-            connected = true;
-            addConsoleText('\r\n' + bleDevice.name + ' Connected.\r\n');
-            send('Connected')
-        })
-        .catch(error => {
-            console.log('' + error);
-        addConsoleText('' + error);
-            if(bleDevice && bleDevice.gatt.connected)
-            {
-                bleDevice.gatt.disconnect();
-            }
-        });
-    }
-
-    function disconnect() {
-        if (!bleDevice) {
-            console.log('No Bluetooth Device connected...');
-            return;
-        }
-        console.log('Disconnecting from Bluetooth Device...');
-        if (bleDevice.gatt.connected) {
-            bleDevice.gatt.disconnect();
-            connected = false;
-            console.log('Bluetooth Device connected: ' + bleDevice.gatt.connected);
-        } else {
-            console.log('> Bluetooth Device is already disconnected');
-        }
-    }
-
-
-    function onDisconnected() {
-        connected = false;
-    addConsoleText('\r\n' + bleDevice.name + ' Disconnected.\r\n');
-    }
-
-    function handleNotifications(event) {
-        console.log('notification');
-        let value = event.target.value;
-        // Convert raw data bytes to character values and use these to 
-        // construct a string.
-        let str = "";
-        for (let i = 0; i < value.byteLength; i++) {
-            str += String.fromCharCode(value.getUint8(i));
-        }
-        
-        addConsoleText(`< RX: ${str}`); 
-    }
-
-    function nusSendString(s) {
-        if(bleDevice && bleDevice.gatt.connected) {
-            console.log("send: " + s);
-            let val_arr = new Uint8Array(s.length)
-            for (let i = 0; i < s.length; i++) {
-                let val = s[i].charCodeAt(0);
-                val_arr[i] = val;
-            }
-            sendNextChunk(val_arr);
-        } else {
-            console.log('Not connected to a device yet.');
-        }
-    }
-
-    function sendNextChunk(a) {
-        let chunk = a.slice(0, MTU);
-        rxCharacteristic.writeValue(chunk)
-        .then(function() {
-            if (a.length > MTU) {
-                sendNextChunk(a.slice(MTU));
-            }
-        });
-    }
-
 // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
@@ -292,7 +168,7 @@ function App() {
 
     function send(data){
         addConsoleText(`> TX: ${data}`)  //add to local console
-        nusSendString(data)             //send over BLE
+        bleMod.nusSendString(data)       //send over BLE
     }
 
     function sendFromInput(data){
@@ -330,10 +206,13 @@ function App() {
 
     useEffect(() => {
         console.log(inputText) 
-        nusSendString(inputText);
+        bleMod.nusSendString(inputText);
         // eslint-disable-next-line react-hooks/exhaustive-deps 
     }, [setInputText])
 
+    function connectionToggle(){
+        bleMod.connectionToggle()
+    }
 
     return (
         <div className='box'>
@@ -342,7 +221,7 @@ function App() {
                 className="button is-primary"
                     onClick={connectionToggle}
                 >
-                    {connected? "Disconnect": "Connect"}
+                    {bleMod.connected? "Disconnect": "Connect"}
                 </button>
                 
                <h1 className="title">BLE UART</h1>
